2cee54e164a2cab945b206973ece8a36db4ee135
Марина <marinal2005.y.h@ya.ru>
2021-03-21 21:42:29 +0300

    создание афиши

---
+++ do_hire.py
@@ -0,0 +1,36 @@
+def make_hire():
+    strok = """{% extends "base.html" %}
+
+{% block content %}
+<h1 style="height:100px; width:1024px; background:#e6a8d7">Афиша</h1>
+<form action="" method="post">
+    <div class="new-list list-place" data-test="list">
+        <div class="new-list__item new-list-place__item" data-test="ITEM">
+"""
+
+    fil = [i.split('|') for i in open('hire.txt', encoding="utf-8").read().split('\n')]
+    for i in range(len(fil)):
+        strok += """
+            <div class="new-list__item-info" style="height:100px; width:1024px; background:#ccccff">
+                <h3>
+                    <a href=""" + """""""" + str(
+            i) + """""""" + """ class="new-list__item-link" data-test="ITEM-URL">""" + fil[i][0] + """</a>"""
+
+        # strok += """\n<img src=""" + f'"photos/{fil[i][1]}"' + """>"""
+        strok += """\n                </h3>
+                    </div>"""
+        print(f'photos/{fil[i][1]}')
+
+    strok += """
+        </div>
+    </div>
+</form>
+{% endblock %}"""
+
+    return strok
+
+
+def load_hire_to_html():
+    f = open("templates/hire.html", 'w', encoding='utf-8')
+    f.write(make_hire())
+    f.close()
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask_login import login_user, login_required, logout_user
 from data.users import User
 from data import db_session
 from flask_login import LoginManager
+from do_hire import load_hire_to_html
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 login_manager = LoginManager()
@@ -51,6 +52,10 @@ def reqister():
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
 
+@app.route('/hire')
+def hire():
+    load_hire_to_html()
+    return render_template('hire.html', title='Aфиша')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
